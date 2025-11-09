eaebefc33937a0b6917398a33fbf5243efb2c9a3
Yee <yeevan@126.com>
2025-11-09 11:58:01 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl font-bold text-green-500">
+      <Button>Click me</Button>
+    </div>
+  );
+}
